209eb72b7fa8b291c74eaae95b0bef0fbbca6795
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-07-08 22:31:00 +0530

    Added Solution for Leetcode-197.Rising Temperature.sql

---
+++ Basic Joins/197. Rising Temperature.sql
@@ -0,0 +1,9 @@
+-- LeetCode 197
+-- Rising Temperature 
+-- Difficulty: Easy
+
+select w1.id
+from Weather w1
+join Weather w2
+on DATEDIFF(w1.recordDate,w2.recordDate)=1
+where w1.temperature>w2.temperature;
